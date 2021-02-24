c4f07c81b2b2ac53419c34077d5154d177a74fb1
Sergyxa <super.serega98@yandex.ru>
2021-02-25 02:44:48 +0300

    Working Project v2

---
+++ game_arg/Physics.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+
+namespace game_arg
+{
+    class Physics
+    {
+        public bool IsCollide(Player player, MapController map, int dirX, int dirY, int ballX, int ballY)
+        {
+            bool isColliding = false;
+            if (player.ballX + dirX > MapController.mapWidth - 1 || ballX + dirX < 0)
+            {
+                player.dirX *= -1;
+                isColliding = true;
+            }
+            else if (player.ballY + dirY < 0)
+            {
+                player.dirY *= -1;
+                isColliding = true;
+            }
+            else if (map.map[ballY + dirY, ballX] != 0)
+            {
+                if (map.map[ballY + dirY, ballX] > 10 && map.map[ballY + dirY, ballX] < 99)
+                {
+                    map.map[ballY + dirY, ballX] = 0;
+                    map.map[ballY + dirY, ballX - 1] = 0;
+                }
+                else if (map.map[ballY + dirY, ballX] < 9)
+                {
+                    map.map[ballY + dirY, ballX] = 0;
+                    map.map[ballY + dirY, ballX + 1] = 0;
+                } 
+                player.dirY *= -1;
+                isColliding = true;
+            }
+            else if (map.map[ballY, ballX + dirX] != 0)
+            { 
+                if (map.map[ballY, ballX + dirX] > 10 && map.map[ballY, ballX + dirX] < 99)
+                {
+                    map.map[ballY, ballX + dirX] = 0;
+                    map.map[ballY, ballX + dirX - 1] = 0;
+                }
+                else if (map.map[ballY, ballX + dirX] < 9)
+                {
+                    map.map[ballY, ballX + dirX] = 0;
+                    map.map[ballY, ballX + dirX + 1] = 0;
+                }
+                player.dirX *= -1;
+                isColliding = true;
+            }
+            return isColliding;
+        }
+    }
+}
---
+++ game_arg/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("game_arg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("game_arg")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cd8c8b65-a646-4f2d-b652-c2bdc6f81eff")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
